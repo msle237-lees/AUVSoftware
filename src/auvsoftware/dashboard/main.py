0d0accdfe538b28a318b0ce5c90e8a100c5af228
Michael Lees <msle237.lees@gmail.com>
2026-03-04 17:46:48 -0500

    started work on other modules

---
+++ src/auvsoftware/dashboard/main.py
@@ -0,0 +1,9 @@
+"""
+src/auvsoftware/dashboard/main.py
+
+This module contains the main entry point and main logic necessary to run the dashboard.
+It is responsible for initializing the dashboard, starting the necessary threads, and handling the main loop of the application.
+"""
+
+from __future__ import annotations
+
